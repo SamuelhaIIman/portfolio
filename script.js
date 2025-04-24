5835460831d4b93b859fa67ac05635689502f13f
Samuel Hållman <samuel.hollman@hotmail.com>
2025-04-25 01:20:41 +0300

    Color fixes, button to the top and logo added

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let text = "";
-if (phone < 1 || phone > 10) {
-    text = "Valid";
-} else {
-    text = "Invalid";
-}
+document.addEventListener("DOMContentLoaded", function () {
+    let mybutton = document.getElementById("backToTop");
 
-let paragraph = document.createElement("p");
-let newContent = document.createTextNode("Hi there and greetings!");
-paragraph.appendChild(newContent);
-let phone = document.getElementById("phone");
-document.body.insertBefore(paragraph, phone);
+    window.onscroll = function () {
+      scrollFunction();
+    };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    mybutton.addEventListener("click", function () {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+  });
